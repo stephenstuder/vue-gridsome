672cf89addb06948f4257c02d64e8c194e43c893
Stephen Studer <stephenkstuder@gmail.com>
2021-04-20 18:33:20 -0700

    update default styling

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,12 +1,12 @@
 <template>
     <body class="layout dark oled">
-        <header class="header">
+        <header>
             <strong>
                 <g-link to="/">{{ $static.metadata.siteName }}</g-link>
             </strong>
-            <nav class="nav">
-                <g-link class="nav__link" to="/">Home</g-link>
-                <g-link class="nav__link" to="/about/">About</g-link>
+            <nav>
+                <g-link to="/">Home</g-link>
+                <g-link to="/about/">About</g-link>
             </nav>
         </header>
         <slot />
